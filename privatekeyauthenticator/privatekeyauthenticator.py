842a95b685db5929000201f2d5fb23f455de67c1
Enziin System <enziinsystem@gmail.com>
2020-10-28 16:10:54 +0700

    Remove comments

--- privatekeyauthenticator/privatekeyauthenticator.py
+++ privatekeyauthenticator/privatekeyauthenticator.py
@@ -11,19 +11,9 @@ class PrivateKeyAuthenticator(Authenticator):
         username = data['username']
         private_key = os.getenv('DATA_SCIENCE_ACCESS_KEY')
 
-        print("****************************************")
-        print("Password hashed: " + password_hashed)
-        print("username: " + username)
-        print("private_key: " + private_key)
-        print("****************************************")
-
         str2hash = username + "-" + private_key
         result = hashlib.md5(str2hash.encode())
         key_hash = result.hexdigest()
-
-        print("****************************************")
-        print("Key hashed: " + key_hash + " == " + password_hashed)
-        print("****************************************")
 
         if key_hash == password_hashed:
             return data['username']
