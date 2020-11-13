77dc1e0b9fad8e9aebda84f9b740d63ccc3ae9f3
Enziin System <enziinsystem@gmail.com>
2020-11-13 16:54:44 +0700

    Add logging to file log

--- privatekeyauthenticator/privatekeyauthenticator.py
+++ privatekeyauthenticator/privatekeyauthenticator.py
@@ -2,6 +2,9 @@ from jupyterhub.auth import Authenticator
 from tornado import gen
 import hashlib
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG, file='/opt/jupyterhub/authentication.log')
 
 
 class PrivateKeyAuthenticator(Authenticator):
@@ -11,19 +14,19 @@ class PrivateKeyAuthenticator(Authenticator):
         username = data['username']
         private_key = os.getenv('DATA_SCIENCE_ACCESS_KEY')
 
-        print("****************************************")
-        print("Password hashed: " + password_hashed)
-        print("username: " + username)
-        print("private_key: " + private_key)
-        print("****************************************")
+        logging.debug("****************************************")
+        logging.debug("Password hashed: " + password_hashed)
+        logging.debug("username: " + username)
+        logging.debug("private_key: " + private_key)
+        logging.debug("****************************************")
 
         str2hash = username + "-" + private_key
         result = hashlib.md5(str2hash.encode())
         key_hash = result.hexdigest()
 
-        print("****************************************")
-        print("Key hashed: " + key_hash + " == " + password_hashed)
-        print("****************************************")
+        logging.debug("****************************************")
+        logging.debug("Key hashed: " + key_hash + " == " + password_hashed)
+        logging.debug("****************************************")
 
         if key_hash == password_hashed:
             return data['username']
